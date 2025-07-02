fb93187a4017c04e53174c66bda34fff8f7c3cb2
Ugo Emekauwa <51210375+ugo-emekauwa@users.noreply.github.com>
2025-07-02 14:28:04 -0400

    Add files via upload

--- nvidia_nim_sample_setup/nim-open-webui-only-setup.sh
+++ nvidia_nim_sample_setup/nim-open-webui-only-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Private AI Setup Dream Guide - Open WebUI Only Setup for NVIDIA NIM (Cisco dCloud Version)
+# Private AI Setup Dream Guide - Open WebUI Only Setup for NVIDIA NIM
 # Written by Ugo Emekauwa (uemekauw@cisco.com, uemekauwa@gmail.com)
 # GitHub Repository: https://github.com/ugo-emekauwa/private-ai-setup-dream-guide
 # Summary: This script sets up an environment with Open WebUI only pre-configured for an NVIDIA NIM demo scenario.
@@ -9,18 +9,16 @@
 # Setup the Script Variables
 echo "Setting up the Script Variables..."
 set -o nounset
+TARGET_HOST=127.0.0.1
+NVIDIA_NIM_1_CONTAINER_HOST_PORT=8005
 OPEN_WEBUI_DEFAULT_MODEL="Mistral NeMo 12B - NIM"
-OPEN_WEBUI_CONTAINER_IMAGE="198.18.134.149:5000/open-webui:cuda"
+OPEN_WEBUI_CONTAINER_IMAGE="ghcr.io/open-webui/open-webui:cuda"
 OPEN_WEBUI_CONTAINER_HOST_PORT=3000
-TARGET_HOST=198.19.5.70
+OPEN_WEBUI_CONTAINER_SPECIFIC_TARGET_HOST="host.docker.internal"
 STOP_AND_REMOVE_PREEXISTING_PRIVATE_AI_CONTAINERS=true
 
 # Start the Open WebUI Only Setup
 echo "Starting the Open WebUI Only Setup..."
-
-# Setup Docker Container Private AI Network
-echo "Setting up Docker Container Private AI Network..."
-sudo docker network create private-ai-setup-network 2>/dev/null
 
 # Stop and Remove Preexisting Private AI Containers
 if $STOP_AND_REMOVE_PREEXISTING_PRIVATE_AI_CONTAINERS; then
@@ -38,12 +36,11 @@ sleep 5
 echo "Setting up the Open WebUI Container..."
 sudo docker run -d \
     --name open-webui-1 \
-    --network private-ai-setup-network \
     -p $OPEN_WEBUI_CONTAINER_HOST_PORT:8080 \
     --gpus all \
     -e WEBUI_AUTH="false" \
     -e WEBUI_NAME="Private AI" \
-    -e OPENAI_API_BASE_URLS="http://mistral-nvidia-nim-1:8000/v1" \
+    -e OPENAI_API_BASE_URLS="http://$OPEN_WEBUI_CONTAINER_SPECIFIC_TARGET_HOST:$NVIDIA_NIM_1_CONTAINER_HOST_PORT/v1" \
     -e OPENAI_API_KEY="mistral-nvidia-nim-1-sample-key" \
     -e DEFAULT_MODELS="$OPEN_WEBUI_DEFAULT_MODEL" \
     -e RAG_EMBEDDING_MODEL="sentence-transformers/paraphrase-MiniLM-L6-v2" \
